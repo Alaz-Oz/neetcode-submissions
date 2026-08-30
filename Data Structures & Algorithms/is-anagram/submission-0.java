ff23fcb268ae9302582716385ad900a36759a7c0
Mohammad Afroz Alam <51114513+Alaz-Oz@users.noreply.github.com>
2026-08-30 20:55:42 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] freqA = new int[26];
+        int[] freqB = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            freqA[s.charAt(i) - 'a']++;
+            freqB[t.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if (freqA[i] != freqB[i]) return false;
+        }
+        return true;
+    }
+}
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int required = target - nums[i];
+            if (seen.containsKey(required)){
+                return new int[]{seen.get(required), i};
+            }
+            seen.put(nums[i], i);
+        }
+        return new int[] {-1, -1}; // Should never reach
+    }
+}
